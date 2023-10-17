040e4f66bf3e55125a996b21792086e3e5f9ebf4
omar farage ibrahim khalil <omrsamir01003@gmail.com>
2023-10-17 14:22:16 +0300

    initial project. add empty home screen.

---
+++ android/app/src/main/kotlin/com/example/asdas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.asdas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:asdas/home screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+    );
+  }
+}
